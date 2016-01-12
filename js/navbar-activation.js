045d7244f700992c2ea6a670365d802df5be4e42
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-01-11 23:20:52 -0500

    Now when the navbar brand is clicked the black background for the active section is removed

--- js/navbar-activation.js
+++ js/navbar-activation.js
@@ -1,22 +1,27 @@
-function addActive(num) {
   var activeItemAbout = document.getElementById("identity0");
   var activeItemPortfolio = document.getElementById("identity1");
   var activeItemChannel = document.getElementById("identity2");
   var activeItemContact = document.getElementById("identity3");
   
-  if (activeItemAbout.classList.contains("active")) {
-    activeItemAbout.classList.remove("active");
-  }
-  if (activeItemPortfolio.classList.contains("active")) {
-    activeItemPortfolio.classList.remove("active");
-  }
-  if (activeItemChannel.classList.contains("active")) {
-    activeItemChannel.classList.remove("active");
-  }
-  if (activeItemContact.classList.contains("active")) {
-    activeItemContact.classList.remove("active");
-  }
+  function removeBlack(){
+    if (activeItemAbout.classList.contains("active")) {
+      activeItemAbout.classList.remove("active");
+    }
+    if (activeItemPortfolio.classList.contains("active")) {
+      activeItemPortfolio.classList.remove("active");
+    }
+    if (activeItemChannel.classList.contains("active")) {
+      activeItemChannel.classList.remove("active");
+    }
+    if (activeItemContact.classList.contains("active")) {
+      activeItemContact.classList.remove("active");
+    }
+  };
 
-  var navbarItem = document.getElementsByTagName("li")[num];
-  navbarItem.className = navbarItem.className + " active";
-};
+  function addActive(num) {
+    removeBlack();  
+    var navbarItem = document.getElementsByTagName("li")[num];
+    navbarItem.className = navbarItem.className + " active";
+  };
+
+   
